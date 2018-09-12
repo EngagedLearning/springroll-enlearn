5773fa630552b1016c52c633f91d691b643142ef
Nick Gravelyn <nick@gravelyn.com>
2018-09-11 19:40:04 -0700

    Replacing custom uuid function with uuid package, along with rollup tooling to bundle it

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import babel from "rollup-plugin-babel";
 import license from "rollup-plugin-license";
 import cleanup from "rollup-plugin-cleanup";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 import path from "path";
 import fs from "fs";
 
 const input = "src/index.js";
 
 const plugins = [
+  resolve({ browser: true }),
+  commonjs(),
   babel({
     exclude: "node_modules/**",
   }),
